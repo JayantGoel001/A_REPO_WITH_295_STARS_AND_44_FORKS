e9caec2d9f36b619e5a7a1640cdbcdc14093d23d
JayantGoel001 <jgoel92@gmail.com>
2021-09-15 22:57:46 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function toOrd(n) {
 
 async function run() {
   try {
-    const githubToken = getInput("githubToken")
-    const actor = getInput("actor") || ""
-    const octokit = getOctokit(githubToken)
+    const githubToken = core.getInput("githubToken")
+    const actor = core.getInput("actor") || ""
+    const octokit = github.getOctokit(githubToken)
     const username = "JayantGoel001"
 
     let filteredRepos = []
@@ -38,19 +38,18 @@ async function run() {
         name: repo.name,
         full_name: repo.full_name,
         stargazers_count: repo.stargazers_count,
-        forks_count : repo.forks_count
       }))
 
       filteredRepos = filteredRepos.concat(
-        data.filter((repo) => repo.name.includes("A-REPO-WITH-"))
+        data.filter((repo) => repo.name.includes("A_REPO_WITH_"))
       )
     }
 
-    filteredRepos.forEach(async ({ full_name, stargazers_count,forks_count }) => {
+    filteredRepos.forEach(async ({ full_name, stargazers_count }) => {
       const [owner, repo] = full_name.split("/")
-      const name = `A-REPO-WITH-${stargazers_count}-STARS-And-${forks_count}-Forks`
-      const title = `A-REPO-WITH-${stargazers_count} STARS ⭐️ And ${forks_count} Forks.`
-      const msg = `[${actor}](https://github.com/${actor}) helped me reach the count of ${toOrd(stargazers_count)} Star and ${toOrd(forks_count)} Fork.`
+      const name = `A_REPO_WITH_${stargazers_count}_STARS`
+      const title = `A_REPO_WITH_${stargazers_count} STARS ⭐️`
+      const msg = `[${actor}](https://github.com/${actor}) helped me count the ${toOrd(stargazers_count)} star, thank you!`
       
       // Break if repo name is not updated
       if (repo == name) { return }
@@ -74,15 +73,19 @@ async function run() {
       
       const readmeContents = [
         `# ${title}`,
-        readmeFile.data.content,
         msg,
+        "## The star count is wrong?",
+        "Then help me fix it. CLICK THE STAR!",
+        "## Want to contribute?",
+        "Clicking the star will trigger the commit which includes the clicker's name to the contributors list. So CLICK THE STAR!",
+        "## [Q&A](https://github.com/narze/THIS_REPO_HAS_X_STARS/wiki/Q&A)",
       ]
 
       await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
         owner,
         repo,
         path: "README.md",
-        message: `⭐️ ${stargazers_count} And ${forks_count} Forks`,
+        message: `⭐️ ${stargazers_count}`,
         content: Buffer.from(readmeContents.join("\n\n")).toString("base64"),
         sha: readmeFile.data.sha,
         author: {
@@ -90,13 +93,13 @@ async function run() {
           email: `${actor}@users.noreply.github.com`,
         },
         committer: {
-          name: "Jayant Goel",
+          name: "Jayant goel",
           email: "jgoel92@gmail.com",
         },
       })
     })
   } catch (error) {
-    setFailed(error.message)
+    core.setFailed(error.message)
   }
 }
 
