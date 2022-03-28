fef2680d7a753672dd30c04c5eabeec795110470
Jayant Goel <jgoel92@gmail.com>
2022-03-28 23:16:59 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -26,11 +26,12 @@ async function run() {
                 i = `[${t}](https://github.com/${t}) helped me reach ${toOrd(o)} stars and ${toOrd(r)} forks.`;
             await n.request("PATCH /repos/{owner}/{repo}", { owner: a, repo: s, name: c });
             const p = await n.request("GET /repos/{owner}/{repo}/contents/{path}", { owner: a, repo: s, path: "README.md" });
-            const m = `# ${u}` + "\n" + new Buffer(p.data.content, "base64").toString().split("\n");
-            console.log("X",new Buffer(p.data.content, "base64").toString());
-            console.log("X",new Buffer(p.data.content, "base64").toString().split("\n").length);
-            console.log("X",m[m.length - 1]);
-            m = m.slice(1).join("\n") + "\n- " + i + "\n";
+            const xy = new Buffer(p.data.content, "base64").toString().split("\n");
+            const m = `# ${u}` + "\n" + xy.slice(1).join("\n") + "\n- " + i + "\n";
+
+            console.log(xy[xy.length - 1]);
+            console.log(xy[xy.length - 2]);
+
             await n.request("PUT /repos/{owner}/{repo}/contents/{path}", {
                 owner: a,
                 repo: s,
